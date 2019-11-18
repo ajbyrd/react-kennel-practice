82e2245861598d9dc456cb0267f075e7ba7e5e4f
Adam Byrd <ajbyrd0723@gmail.com>
2019-11-18 15:03:09 -0600

    adding other links

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,34 +1,17 @@
 import React, { Component } from 'react'
 import './Kennel.css'
-import AnimalCard from './animal/AnimalCard'
-import EmployeeCard from './employees/EmployeeCard'
-import OwnerCard from './owners/OwnerCard';
-import LocationCard from './locations/LocationCard';
+
 import NavBar from './nav/NavBar';
+import ApplicationViews from './ApplicationViews';
+
 
 class Kennel extends Component {
     render() {
         return (
-            <div>
+            <React.Fragment>
                 <NavBar />
-                <div>
-                    <h2>Student Kennels<br />
-                        <small>Loving care when you're not there.</small>
-                    </h2>
-                    <address>
-                        Visit Us at the Downtown Location
-                    <br />123 Broadway Street
-                </address>
-                </div>
-                <div>
-                    <AnimalCard />
-                    <OwnerCard />
-                    <LocationCard />
-                    <EmployeeCard />
-                    
-                    
-                </div>
-            </div>
+                <ApplicationViews />
+            </React.Fragment>
         );
     }
 }
